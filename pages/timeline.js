75f2feca23430b838c93d922e00170285e938c0d
yanglijie <2636984421@qq.com>
2022-01-20 15:48:18 +0800

    111

---
+++ pages/timeline.js
@@ -0,0 +1,70 @@
+// pages/timeline.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    lineArr:[
+      {name:'待处理'},
+      {name:'派单',time:'2022-01-18 18:24:10',title:'派单张三'},
+      {name:'产生',time:'2022-01-18 16:23:10',title:'额定电压过大额定电压过大额定电压过大额定电压过大额定电压过大额定电压过大额定电压过大'},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
